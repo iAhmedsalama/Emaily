5dae547cd4685c79dd1fbce9c0794a1456ffd230
Ahmed Salama <ahmed.s.khalil@hotmail.com>
2022-03-12 00:20:59 +0200

    adding client module

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+    app.use(
+        createProxyMiddleware('/auth/google', {
+            target: 'http://localhost:5000',
+            secure: false,
+        }),
+    );
+};
